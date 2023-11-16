290ccc8a416156a5e45a68c6a5aa1a6e2e4aa082
DESKTOP-PKK6FUL\Kapral Klitorchuk <vitchenko.kirill@gmail.com>
2023-11-16 22:39:09 +0200

    add url to allowlist

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withNextIntl = require("next-intl/plugin")("./i18n.ts");
 
 module.exports = withNextIntl({
   images: {
-    domains: ["127.0.0.1"],
+    domains: ["127.0.0.1", "https://ngep-uploads.s3.eu-north-1.amazonaws.com"],
   },
 });
